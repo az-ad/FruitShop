461f3c158a84c4f5a120b9a5e97ea9454009a976
az-ad <tasmim.cu.cse567@gmail.com>
2023-06-12 14:43:43 +0600

    null

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,18 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: "AIzaSyBreqMg2YjZ90sdygvelEUvlvt369LJ5Wk",
-    // authDomain: "azad-fruit-shop.firebaseapp.com",
-    // projectId: "azad-fruit-shop",
-    // storageBucket: "azad-fruit-shop.appspot.com",
-    // messagingSenderId: "830473569211",
-    // appId: "1:830473569211:web:1b96e651d9203fa17dba9e"
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    apiKey: "AIzaSyBreqMg2YjZ90sdygvelEUvlvt369LJ5Wk",
+    authDomain: "azad-fruit-shop.firebaseapp.com",
+    projectId: "azad-fruit-shop",
+    storageBucket: "azad-fruit-shop.appspot.com",
+    messagingSenderId: "830473569211",
+    appId: "1:830473569211:web:1b96e651d9203fa17dba9e"
+    // apiKey: process.env.REACT_APP_apiKey,
+    // authDomain: process.env.REACT_APP_authDomain,
+    // projectId: process.env.REACT_APP_projectId,
+    // storageBucket: process.env.REACT_APP_storageBucket,
+    // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    // appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
